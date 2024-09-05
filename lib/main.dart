14ca9594877fc0e68ac90d94269ec5f7b41d54b4
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-05 17:41:14 +0300

    Create project file

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LoginRegisterFormUI());
+}
+
+class LoginRegisterFormUI extends StatelessWidget {
+  const LoginRegisterFormUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
